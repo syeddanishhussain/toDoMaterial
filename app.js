215fdda920bcb5260512265b0e66fc1ac6c2631f
syeddanishhussain <syeddanish_hussain@hotmail.com>
2016-01-05 16:50:31 +0500

    function

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.controller("AppController",function(){
 
 
 
-    //Add todos
+
     this.addItem = function()
     {
         this.allItem.push(
@@ -25,3 +25,39 @@ app.controller("AppController",function(){
         this.todoItem = "";
     }
 })
+
+    this.delete = function()
+    {
+        for(var i = 0;i<this.allItem.length;i++)
+        {
+            if(this.allItem[i].check == true)
+        {
+                this.allItem.splice(i,1);
+                i--;
+                this.counter--;
+        }
+
+    }
+    };
+
+
+
+    this.counter = 0;
+
+
+    this.selected= function(a)
+    {
+        if(this.allItem[a].check == true)
+        {
+            this.counter--;
+        }
+            else
+        {
+            this.counter++;
+        }
+
+    }
+
+
+
+
